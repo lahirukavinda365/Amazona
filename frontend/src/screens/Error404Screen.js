4a5b33b04d401b0f29786adf0e6969b19c38fb63
Lahiru Kavinda <lahirukavinda365@gmail.com>
2023-04-13 12:40:13 +0700

    URL router build completed

---
+++ frontend/src/screens/Error404Screen.js
@@ -0,0 +1,16 @@
+
+const Error404Screen = {
+    render: () => {
+        return `<div>Page not Found!</div>`;
+    },
+};
+export default Error404Screen;
+
+
+
+
+
+
+
+
+
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,16 @@
+
+const ProductScreen = {
+    render: () => {
+        return `<div>ProductScreen</div>`;
+    },
+};
+export default ProductScreen;
+
+
+
+
+
+
+
+
+
